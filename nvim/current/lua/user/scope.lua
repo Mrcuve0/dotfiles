a872c0925b9462df098c0e4c6d575a694d413988
mrcuve0 <mrcuve0@posteo.net>
2022-09-06 14:27:13 +0200

    Add old nvchad config for reference

--- nvim/current/lua/user/scope.lua
+++
@@ -1,9 +0,0 @@
-local status_ok, scope = pcall(require, "scope")
-if not status_ok then
-  vim.notify("Error while loading 'scope.lua', returning...")
-  return
-end
-
-local options = {}
-
-scope.setup(options)
